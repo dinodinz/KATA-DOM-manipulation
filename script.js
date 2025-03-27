0e533d5b8d31e9685328b5080f743e08bab4dff3
dinodin <dinodin@dinodins-MacBook-Pro.local>
2025-03-27 01:00:19 +0000

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log('hello from index.js!');
+const remainingTime = document.getElementById("remaining-time");
+const addTime = document.getElementsByClassName("add-time-button");
+const startButton = document.getElementById("start-button");
+const resetButton = document.getElementById("reset-button");
+let beginTimer = 0;
+let timeRunning = false;
+
+[...addTime].forEach((button) => {
+  button.addEventListener("click", () => {
+    updateSeconds(+button.value);
+    startButton.disabled = false;
+  });
+});
+
+window.addEventListener("keydown", (event) => {
+  if (event.key == "s" && remainingTime.textContent !== "0") {
+    if (!timeRunning && remainingTime.textContent !== "0") {
+      beginTimer = setInterval(runTimer, 1000);
+      startButton.textContent = "Pause";
+    } else {
+      clearInterval(beginTimer);
+      timeRunning = false;
+      startButton.textContent = "Resume";
+    }
+  }
+});
+
+startButton.addEventListener("click", (event) => {
+  if (!timeRunning && remainingTime.textContent !== "0") {
+    beginTimer = setInterval(runTimer, 1000);
+    startButton.textContent = "Pause";
+  } else {
+    clearInterval(beginTimer);
+    timeRunning = false;
+    startButton.textContent = "Resume";
+  }
+});
+
+resetButton.addEventListener("click", () => {
+  remainingTime.textContent = 0;
+  timeRunning = false;
+  startButton.textContent = "Start";
+  startButton.disabled = true;
+  clearInterval(beginTimer);
+});
+
+function updateSeconds(seconds) {
+  remainingTime.textContent = +remainingTime.textContent + seconds;
+}
+
+function runTimer() {
+  if (+remainingTime.textContent === 0) {
+    startButton.textContent = "Start";
+    timeRunning = false;
+    startButton.disabled = true;
+    clearInterval(beginTimer);
+    console.log("Timer stopped!");
+  } else {
+    timeRunning = true;
+    console.log("minus 1 second");
+    remainingTime.textContent = +remainingTime.textContent - 1;
+  }
+}
